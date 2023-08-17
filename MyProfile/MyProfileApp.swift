5060141ed1926ee444fc667321fd215dbb1d38a5
Alissa Rogozin <alissa.rogozin@gmail.com>
2023-08-17 13:12:19 -0400

    Initial Commit

---
+++ MyProfile/MyProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProfileApp.swift
+//  MyProfile
+//
+//  Created by scholar on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MyProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
